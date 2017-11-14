0ba9d4877939f87448aec010c797153ce5bc2618
cmho <carlymho@gmail.com>
2017-11-14 00:32:41 -0600

    add comment styling, polish post pages

--- resources/assets/scripts/routes/common.js
+++ resources/assets/scripts/routes/common.js
@@ -1,9 +1,10 @@
 export default {
   init() {
     // JavaScript to be fired on all pages
-    $('input[type="submit"]').wrap("<div class='button-wrapper'></div>");
+    
   },
   finalize() {
     // JavaScript to be fired on all pages, after page specific JS is fired
+    $('input[type="submit"]').wrap("<div class='button-wrapper'></div>");
   },
 };
