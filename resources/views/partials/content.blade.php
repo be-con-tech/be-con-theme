da12741bfcffe54622f0f3a03174b4d222bf3d61
cmho <carlymho@gmail.com>
2017-11-16 22:48:37 -0600

    set default image for blog posts

--- resources/views/partials/content.blade.php
+++ resources/views/partials/content.blade.php
@@ -1,7 +1,13 @@
 <div @php(post_class('wrapper blog-entry'))>
   <div class="row middle-xs">
-    <div class="col-md-3 col-xs-12">
-      {{ get_the_post_thumbnail('medium') }}
+    <div class="col-md-3 col-xs-12 image">
+      @if(has_post_thumbnail())
+        {{ get_the_post_thumbnail('medium') }}
+      @else
+        <div class="default-image">
+          <img src="{{ App\asset_path('images/logo-light.png') }}" />
+        </div>
+      @endif
     </div>
     <div class="col-md-9 col-xs-12">
       <h2><a href="{{ get_permalink() }}">{{ get_the_title() }}</a></h2>
