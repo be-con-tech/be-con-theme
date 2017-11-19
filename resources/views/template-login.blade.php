5e8de95fb67f5a5a866231065e88dbf693582e7f
cmho <carlymho@gmail.com>
2017-11-19 14:31:37 -0600

    remove references to oauth until I can implement it

--- resources/views/template-login.blade.php
+++ resources/views/template-login.blade.php
@@ -10,10 +10,10 @@
             <div class="row">
                 <div class="col-md-8 col-md-offset-2 col-xs-12">
                     <h2>Sign In</h2>
-                    <div class="oauth button-row center">
+                    <!--<div class="oauth button-row center">
                         <a href="#" class="button oauth-button twitter"><i class="fa fa-twitter"></i> Log in with Twitter</a> <a href="#" class="button oauth-button facebook"><i class="fa fa-facebook"></i> Log in with Facebook</a>
                     </div>
-                    <p class="or">Or</p>
+                    <p class="or">Or</p>-->
                     <?php wp_login_form(array(
                         'redirect' => get_bloginfo('url').'/account/'
                     )); ?>
